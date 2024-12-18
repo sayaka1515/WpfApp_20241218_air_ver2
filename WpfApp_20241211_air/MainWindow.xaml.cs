30ae24b9342872fbcad5c62ef1eaccf4b49b5bb4
owner <owner@C301-29>
2024-12-18 08:18:01 +0800

    加入專案檔案。

---
+++ WpfApp_20241211_air/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_20241211_air
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string aqiURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
+        public MainWindow()
+        {
+            InitializeComponent();
+            urlTextBox.Text = aqiURL;
+        }
+
+        private async void btnGetAQI_Click(object sender, RoutedEventArgs e)
+        {
+            string url = urlTextBox.Text;
+            ContentTextBox.Text = "抓取資料中...";
+
+            string data = await GetAQIAsync(url);
+            ContentTextBox.Text = data;
+        }
+
+        private async Task<string> GetAQIAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+        }
+    }
+}
